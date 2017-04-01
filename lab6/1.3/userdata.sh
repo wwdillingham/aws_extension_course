74d2a39232a561445e3aadede4a725b504dad0e4
Wes <wes_dillingham@harvard.edu>
2017-04-01 10:54:48 -0400

    userdata for lab6 case 1 part 3

---
+++ lab6/1.3/userdata.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+yum -y update
+yum -y install httpd php mysql php-mysql
+chkconfig httpd on
+/etc/init.d/httpd start
+if [ ! -f /var/www/html/lab2-app.tar.gz ]; then
+cd /var/www/html
+wget https://s3-us-west-2.amazonaws.com/us-west-2-aws-staging/awsu-ilt/academy-cca/v3.0/labs/lab5-rds/scripts/lab5-app.tar.gz 
+chown apache:root /var/www/html/rds.conf.php
+fi 
+
