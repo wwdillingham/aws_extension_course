6c3a585f1bbd516134617ae3b79950931e81b601
Wes <wes_dillingham@harvard.edu>
2017-04-01 13:12:00 -0400

    add userdata to formation

--- lab6/1.3/userdata.sh
+++ lab6/1.3/userdata.sh
@@ -1,11 +1,12 @@
-#!/bin/bash -ex
-yum -y update
-yum -y install httpd php mysql php-mysql
-chkconfig httpd on
-/etc/init.d/httpd start
-if [ ! -f /var/www/html/lab2-app.tar.gz ]; then
-cd /var/www/html
-wget https://s3-us-west-2.amazonaws.com/us-west-2-aws-staging/awsu-ilt/academy-cca/v3.0/labs/lab5-rds/scripts/lab5-app.tar.gz 
-chown apache:root /var/www/html/rds.conf.php
-fi 
+"#!/bin/bash -ex\n"
+"touch /tmp/userdata_started\n"
+"yum -y update\n"
+"yum -y install httpd php mysql php-mysql\n"
+"chkconfig httpd on"
+"/etc/init.d/httpd start\n"
+"if [ ! -f /var/www/html/lab2-app.tar.gz ]; then\n"
+"cd /var/www/html\n"
+"wget https://s3-us-west-2.amazonaws.com/us-west-2-aws-staging/awsu-ilt/academy-cca/v3.0/labs/lab5-rds/scripts/lab5-app.tar.gz\n" 
+"chown apache:root /var/www/html/rds.conf.php\n"
+"fi\n" 
 
