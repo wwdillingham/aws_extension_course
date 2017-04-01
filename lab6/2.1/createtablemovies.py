c8016ff2070401fb57efc5e14f411565828f44db
Wes <wes_dillingham@harvard.edu>
2017-04-01 15:32:07 -0400

    bring it createtablemovies.py with updated endpoint

---
+++ lab6/2.1/createtablemovies.py
@@ -0,0 +1,47 @@
+from __future__ import print_function # Python 2/3 compatibility
+# The following two lines are a fix for Windows boto3 issue
+import os
+os.environ["TZ"]="UTC"
+import boto3
+import sys
+
+dynamodb = boto3.resource('dynamodb',endpoint_url='https://dynamodb.us-east-1.amazonaws.com')
+
+try:
+    table = dynamodb.create_table(
+    TableName='Movies',
+    KeySchema=[
+        {
+            'AttributeName': 'year',
+            'KeyType': 'HASH'  #Partition key
+        },
+        {
+            'AttributeName': 'title',
+            'KeyType': 'RANGE'  #Sort key
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'year',
+            'AttributeType': 'N'
+        },
+        {
+            'AttributeName': 'title',
+            'AttributeType': 'S'
+        },
+
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 1,
+        'WriteCapacityUnits': 1
+    }
+)
+    print("table created")
+
+except OSError as err:
+    print("OS error: {0}".format(err))
+except ValueError:
+    print("Could not convert data to an integer.")
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
