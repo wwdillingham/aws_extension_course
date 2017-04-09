3f09fb1b054b4634ef4d35177440165310588e45
Wes <wes_dillingham@harvard.edu>
2017-04-09 12:04:40 -0400

    create a bucket

---
+++ lab7/acme-wdillingham-create-bucket.py
@@ -0,0 +1,17 @@
+#wes dillingham
+#list all remote buckets
+
+import boto
+import boto.s3.connection
+
+conn = boto.connect_s3(
+        calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+        )
+
+bucket = conn.create_bucket('my-new-bucket')
+
+for bucket in conn.get_all_buckets():
+        print "{name}\t{created}".format(
+                name = bucket.name,
+                created = bucket.creation_date,
+        )
