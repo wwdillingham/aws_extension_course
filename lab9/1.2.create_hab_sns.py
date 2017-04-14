fd0fac086e6b5522e605e7035283c5d00d3c9c19
Wes <wes_dillingham@harvard.edu>
2017-04-14 11:25:01 -0400

    remove comments

--- lab9/1.2.create_hab_sns.py
+++ lab9/1.2.create_hab_sns.py
@@ -1,6 +1,3 @@
-#Create 1 SNS topic in Python called: HighAccountBalanceAlertSNSTopic with attribute DisplayName=HABTopic.  CCA Lab 12, Steps 11 – 15.  Do this in Python.
-#Show your code, your SNS Topic and your ARN from Console or from Python.
-
 import boto3
 client = boto3.client('sns')
 
