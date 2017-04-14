0493083be9c620d7c12fc9c6c0ec3a8a2b41d51a
Wes <wes_dillingham@harvard.edu>
2017-04-14 16:25:57 -0400

    wrong arn

--- lab9/1.5.subscribe.to.queues.py
+++ lab9/1.5.subscribe.to.queues.py
@@ -1,12 +1,15 @@
-# arn:aws:sqs:us-east-1:644009940612:UserNotificationQueue
-#arn:aws:sqs:us-east-1:644009940612:CreditCollectionQueue
-
 import boto3
 client = boto3.client('sns')
 
-subscription_response = client.subscribe(
+notification_subscription_response = client.subscribe(
     TopicArn='arn:aws:sqs:us-east-1:644009940612:UserNotificationQueue',
     Protocol='email',
     Endpoint='wes_dillingham@harvard.edu'
 )
+collection_subscription_response = client.subscribe(
+    TopicArn='arn:aws:sqs:us-east-1:644009940612:CreditCollectionQueue',
+    Protocol='email',
+    Endpoint='wes_dillingham@harvard.edu'
+)
 print(subscription_response)
+print(collection_subscription_response)
