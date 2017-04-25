3941ea1aaaa0d258555f437a6ce1dac00dad9b92
Wes <wes_dillingham@harvard.edu>
2017-04-25 18:27:37 -0400

    lets do these creates 1 per file

--- lab9/1.6.create.3.dyndb.tables.py
+++ lab9/1.6.create.3.dyndb.tables.py
@@ -31,30 +31,3 @@ ProvisionedThroughput={
 }
 )
 
-table2 = dynamodb.create_table(
-TableName='Transactions',
-KeySchema=[
-    {
-        'AttributeName': 'TransactionId',
-        'KeyType': 'HASH'
-    },
-    {
-        'AttributeName': 'TransactionId',
-        'KeyType': 'RANGE'
-    }
-],
-AttributeDefinitions=[
-    {
-        'AttributeName': 'CustomerId',
-        'AttributeType': 'S'
-    },
-],
-ProvisionedThroughput={
-    'ReadCapacityUnits': 5,
-    'WriteCapacityUnits': 5
-},
-StreamSpecification={
-    'StreamEnabled': True|False,
-    'StreamViewType': 'NEW_IMAGE'
-    }
-)
