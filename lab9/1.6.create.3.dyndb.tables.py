4933545832a3976854f130b022e53f81e31dfde8
Wes <wes_dillingham@harvard.edu>
2017-04-24 22:20:44 -0400

    part 1 of 1.6

---
+++ lab9/1.6.create.3.dyndb.tables.py
@@ -0,0 +1,21 @@
+import boto3
+
+
+#Define a table called Customer with a Primary partition key called CustomerId  as a String.  Note: There is no Primary sort key.
+#Define a table called Transactions with a Primary partition key called CustomerId  as a String and Primary sort key called TransactionId as a String.
+#Define a table called TransactionTotal  with a Primary partition key called CustomerId  as a String
+#Note: There is no Primary sort key.	
+#Also for all 3 tables Set Provisioned read capacity units and Provisioned write capacity units =5.
+
+
+dynamodb = boto3.resource('dynamodb',endpoint_url='https://dynamodb.us-east-1.amazonaws.com')
+
+try:
+    table = dynamodb.create_table(
+    TableName='Customer',
+    KeySchema=[
+        {
+            'AttributeName': 'CustomerId',
+            'KeyType': 'STRING' 
+        }
+)
