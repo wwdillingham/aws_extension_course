e13cd907d3b3626b5990e2fad695fad6a8c77fdf
Wes <wes_dillingham@harvard.edu>
2017-04-25 18:08:49 -0400

    1.6 part 2

--- lab9/1.6.create.3.dyndb.tables.py
+++ lab9/1.6.create.3.dyndb.tables.py
@@ -1,4 +1,3 @@
-import boto3
 
 
 #Define a table called Customer with a Primary partition key called CustomerId  as a String.  Note: There is no Primary sort key.
@@ -7,6 +6,7 @@ import boto3
 #Note: There is no Primary sort key.	
 #Also for all 3 tables Set Provisioned read capacity units and Provisioned write capacity units =5.
 
+import boto3
 
 dynamodb = boto3.resource('dynamodb',endpoint_url='https://dynamodb.us-east-1.amazonaws.com')
 
@@ -16,7 +16,7 @@ TableName='Customer',
 KeySchema=[
     {
         'AttributeName': 'CustomerId',
-        'KeyType': 'HASH' 
+        'KeyType': 'RANGE'
     }
 ],
 AttributeDefinitions=[
@@ -29,4 +29,32 @@ ProvisionedThroughput={
     'ReadCapacityUnits': 5,
     'WriteCapacityUnits': 5
 }
+)
+
+table2 = dynamodb.create_table(
+TableName='Transactions',
+KeySchema=[
+    {
+        'AttributeName': 'CustomerId',
+        'KeyType': 'RANGE'
+    },
+    {
+        'AttributeName': 'TransactionId',
+        'KeyType': 'RANGE'
+    }
+],
+AttributeDefinitions=[
+    {
+        'AttributeName': 'CustomerId',
+        'AttributeType': 'S'
+    },
+],
+ProvisionedThroughput={
+    'ReadCapacityUnits': 5,
+    'WriteCapacityUnits': 5
+},
+StreamSpecification={
+    'StreamEnabled': True|False,
+    'StreamViewType': 'NEW_IMAGE'
+    }
 )
