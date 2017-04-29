074ffcdb47781d0eb52a930de618a2999e208cf3
Wes <wes_dillingham@harvard.edu>
2017-04-29 17:38:00 -0400

    variable name conflict

--- lab10/2.1.create.bucket.py
+++ lab10/2.1.create.bucket.py
@@ -9,7 +9,7 @@ s3.create_bucket(Bucket='acme-source-wdillingham-resized')
 
 #upload to lab 1 bucket
 lab1bucket = 'acme-source-wdillingham'
-bucket = s3.Bucket(lab1bucket)
+newbucket = s3.Bucket(lab1bucket)
 
 data = open('HappyFace.jpg', 'rb')
 s3.Bucket(lab1bucket).put_object(Key='HappyFace.jpg', Body=data)
@@ -19,6 +19,6 @@ for bucket in s3.buckets.all():
         print(bucket.name)
 
 #list objects in that bucket
-for object in bucket.objects.all():
+for object in newbucket.objects.all():
     print(object)
 
