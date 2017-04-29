06469edea33e71b8b4c5afe88f812d05da0b1066
Wes <wes_dillingham@harvard.edu>
2017-04-29 12:19:06 -0400

    only apply policy to the one bucket

--- lab10/1.1.createbuckets.py
+++ lab10/1.1.createbuckets.py
@@ -11,16 +11,11 @@ provided_policy = './policy.json'
 with open(provided_policy, 'r') as policy:
         the_policy = json.load(policy)
 
-response1 = client.put_bucket_policy(
-    Bucket='acme-source-wdillingham',
-    Policy='string'
-)
-response2 = client.put_bucket_policy(
+policy_response = client.put_bucket_policy(
     Bucket='acme-logs-wdillingham',
     Policy=json.dumps(the_policy)
 )
-print(response1)
-print(response2)
+print(policy_response)
 #list buckets
 for bucket in s3.buckets.all():
         print(bucket.name)
