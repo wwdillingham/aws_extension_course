e8e898314da2481df831600e89ff6ace322597f9
Wes <wes_dillingham@harvard.edu>
2017-04-29 17:02:14 -0400

    make bucket a var

--- lab10/1.7.upload.to.s3.py
+++ lab10/1.7.upload.to.s3.py
@@ -3,4 +3,5 @@ import boto3
 s3 = boto3.resource('s3')
 
 data = open('foo.txt', 'rb')
-s3.Bucket(acme-source-wdillingham).put_object(Key='foo.txt', Body=data)
+bucket='acme-source-wdillingham'
+s3.Bucket(bucket).put_object(Key='foo.txt', Body=data)
