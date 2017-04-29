3c0d9365690511370c450bc1ff658ebf464a39da
Wes <wes_dillingham@harvard.edu>
2017-04-29 17:06:29 -0400

    need to upload this other file as well

--- lab10/1.7.upload.to.s3.py
+++ lab10/1.7.upload.to.s3.py
@@ -2,6 +2,6 @@ import boto3
 
 s3 = boto3.resource('s3')
 
-data = open('foo.txt', 'rb')
+data = open('foo2.txt', 'rb')
 bucket='acme-source-wdillingham'
-s3.Bucket(bucket).put_object(Key='foo.txt', Body=data)
+s3.Bucket(bucket).put_object(Key='foo2.txt', Body=data)
