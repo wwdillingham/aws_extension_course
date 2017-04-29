c645a69c0e3d63187f840707d14b1ff332f28959
Wes <wes_dillingham@harvard.edu>
2017-04-29 17:33:35 -0400

    correct filename

--- lab10/2.1.create.bucket.py
+++ lab10/2.1.create.bucket.py
@@ -9,8 +9,8 @@ s3.create_bucket(Bucket='acme-source-wdillingham-resized')
 
 #upload to lab 1 bucket
 lab1bucket = 'acme-source-wdillingham'
-data = open('HappyFace.jph', 'rb')
-s3.Bucket(lab1bucket).put_object(Key='848px-Tux_Mono.svg.png', Body=data)
+data = open('HappyFace.jpg', 'rb')
+s3.Bucket(lab1bucket).put_object(Key='HappyFace.jpg', Body=data)
 
 #List all buckets
 for bucket in s3.buckets.all():
