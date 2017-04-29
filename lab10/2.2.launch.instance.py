13429c78b22cb61c448f04b685456f8fda8e77e1
Wes <wes_dillingham@harvard.edu>
2017-04-29 18:01:05 -0400

    add security groups

--- lab10/2.2.launch.instance.py
+++ lab10/2.2.launch.instance.py
@@ -4,5 +4,9 @@ client = boto3.client('ec2')
 
 response = client.run_instances(
   ImageId='ami-0b33d91d',
-  InstanceType='t2.micro'
+  InstanceType='t2.micro',
+  SecurityGroups=[
+          'sg-a06f88df',
+      ],
 )
+
