2bd65ce3f61f481972a56c312a4a3005380db221
Wes <wes_dillingham@harvard.edu>
2017-04-29 17:32:29 -0400

    2.1 rought

---
+++ lab10/2.1.create.bucket.py
@@ -0,0 +1,22 @@
+#create a bucket and list buckets
+import json
+import boto3
+s3 = boto3.resource('s3')
+client = boto3.client('s3')
+
+#create bucket
+s3.create_bucket(Bucket='acme-source-wdillingham-resized')
+
+#upload to lab 1 bucket
+lab1bucket = 'acme-source-wdillingham'
+data = open('HappyFace.jph', 'rb')
+s3.Bucket(lab1bucket).put_object(Key='848px-Tux_Mono.svg.png', Body=data)
+
+#List all buckets
+for bucket in s3.buckets.all():
+        print(bucket.name)
+
+#list objects in that bucket
+for object in lab1bucket.objects.all():
+    print(object)
+
