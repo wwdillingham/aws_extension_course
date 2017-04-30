894e9763d3a0cbb92284968e0c5d8773db2ec964
Wes <wes_dillingham@harvard.edu>
2017-04-29 22:03:22 -0400

    list objects

---
+++ lab10/2.10.list.objects.py
@@ -0,0 +1,17 @@
+#create a bucket and list buckets
+import json
+import boto3
+s3 = boto3.resource('s3')
+client = boto3.client('s3')
+
+
+lab1bucket = 'acme-source-wdillingham'
+newbucket = s3.Bucket(lab1bucket)
+
+#List all buckets
+for bucket in s3.buckets.all():
+        print(bucket.name)
+
+#list objects in that bucket
+for object in newbucket.objects.all():
+    print(object)
