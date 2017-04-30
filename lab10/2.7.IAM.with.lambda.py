c40657c26e6afd86f7c1d0c4ff8789db47ed3a0f
Wes <wes_dillingham@harvard.edu>
2017-04-29 20:07:09 -0400

    json fiX

--- lab10/2.7.IAM.with.lambda.py
+++ lab10/2.7.IAM.with.lambda.py
@@ -4,15 +4,15 @@ client = boto3.client('iam')
 response = client.create_role(
     RoleName='lambda-s3-execution-role',
     AssumeRolePolicyDocument='''
-    {
-    	"Version": "2012-10-17",
-    	"Statement": [{
-    		"Sid": "",
-    		"Effect": "Allow",
-    		"Principal": {
-    			"Service": "lambda.amazonaws.com"
-    		},
-    		"Action": "sts:AssumeRole"
-    	}]
-    } '''
+{
+	"Version": "2012-10-17",
+	"Statement": [{
+		"Sid": "",
+		"Effect": "Allow",
+		"Principal": {
+			"Service": "lambda.amazonaws.com"
+		},
+		"Action": "sts:AssumeRole"
+	}]
+} '''
 )
