8134147a5e9abd9405c7a1a865fe8813f970fab9
Wes <wes_dillingham@harvard.edu>
2017-05-09 22:13:06 -0400

    get outputs in there

--- final/5.video.transcode.py
+++ final/5.video.transcode.py
@@ -42,8 +42,10 @@ pipeline_response = transcode_client.create_pipeline(
 pipeline_id=pipeline_response['Pipeline']['Id']
 job_response = transcode_client.create_job(
     PipelineId=pipeline_id,
-    OutputKeyPrefix='output.mp4',
     Input={
         'Key': 'Wildlife.wmv',
+        },
+    Output={
+        'Key': 'output.wmv',
         }
 )
