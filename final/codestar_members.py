1f32b9f88c23a934f6881df36b692cdfb149beff
Wes <wes_dillingham@harvard.edu>
2017-05-10 22:21:34 -0400

    associate team members

--- final/codestar_members.py
+++ final/codestar_members.py
@@ -1,9 +1,12 @@
 #arn:aws:codestar:us-east-1:644009940612:project/wdillingham-fin
-
 import boto3
 
 client = boto3.client('codestar')
 iam_client = boto3.client('iam')
+
+me = iam_client.describe_user(
+    UserName='wdillingham'
+)
 
 create1response = iam_client.create_user(
     UserName='diane-codestar'
@@ -11,10 +14,28 @@ create1response = iam_client.create_user(
 create2response = iam_client.create_user(
     UserName='zhen-codestar'
 )
+arn_diane = create1response['User']['Arn']
+arn_zhen = create2response['User']['Arn']
+arn_me = me['User']['Arn']
+print(arn_me)
 
+associate_me = associate_team_member(
+    projectId = 'wdillingham-fin',
+    userArn = arn_me,
+    projectRole = 'Owner'
+)
+associate_diane = associate_team_member(
+    projectId = 'wdillingham-fin',
+    userArn = arn_diane,
+    projectRole = 'Contributor'
+)
+associate_zhen = associate_team_member(
+    projectId = 'wdillingham-fin',
+    userArn = arn_zhen,
+    projectRole = 'Contributor'
+)
 list_response = client.list_team_members(
     projectId='wdillingham-fin',
     )
 
-arn_1 = create1response['User']['Arn']
-print(arn_1)
+print(list_response)
