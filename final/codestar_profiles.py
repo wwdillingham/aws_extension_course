42edba7100cd9a1b8d629f76c110182536ecdc14
Wes <wes_dillingham@harvard.edu>
2017-05-10 22:59:49 -0400

    email

--- final/codestar_profiles.py
+++ final/codestar_profiles.py
@@ -19,14 +19,17 @@ arn_me = me['User']['Arn']
 me_response = client.create_user_profile(
     userArn=arn_me,
     displayName='Wes Dillingham',
+    emailAddress='wes_dillingham@harvard.edu'
 )
 zhen_response = client.create_user_profile(
     userArn=arn_zhen,
     displayName='Zhenyu Zhao',
+    emailAddress='fake_1@harvard.edu'
 )
 diane_response = client.create_user_profile(
     userArn=arn_diane,
     displayName='Diane Howard',
+    emailAddress='fake_2@harvard.edu'
 )
 
 describe_me = client.describe_user_profile(
