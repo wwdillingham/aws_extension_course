72a36343643b67d3db09e3799eb8766b127b50fb
Wes <wes_dillingham@harvard.edu>
2017-05-12 08:49:26 -0400

    8

--- final/s3_polly_glacier.py
+++ final/s3_polly_glacier.py
@@ -8,7 +8,7 @@ s3_client = boto3.client('s3')
 cf_client = boto3.client('cloudformation')
 glacier_client = boto3.client('glacier')
 cf_bucket = 'cloudformation-input-bucket'
-target_bucket = 'neighborhood-development-translation-bucket7'
+target_bucket = 'neighborhood-development-translation-bucket8'
 
 s3_resource.create_bucket(Bucket=cf_bucket) #this is the bucket where cloudformation JSON objects live
 data = open('s3_cf.json', 'rb')
@@ -17,7 +17,7 @@ s3_resource.Bucket(cf_bucket).put_object(Key='s3_cf.json', Body=data)
 
 # create the stack (which in turn creates an s3 bucket for future polly files)
 cf_response = cf_client.create_stack(
-    StackName='neighborhood-development-stack7',
+    StackName='neighborhood-development-stack8',
     TemplateURL='https://s3.amazonaws.com/cloudformation-input-bucket/s3_cf.json'
     )
 time.sleep(60)
@@ -65,8 +65,8 @@ vault_creation_response = glacier_client.create_vault(
 #upload to glacier vault
 vault_bucket = s3_resource.Bucket(target_bucket) 
 for object in vault_bucket.objects.all():
+    print(object)
     with open(object, 'rb') as archive:
-        print(object)
         glacier_response = glacier_client.upload_archive(
             vaultName=target_bucket,
             archiveDescription='neighborhood services translation',
