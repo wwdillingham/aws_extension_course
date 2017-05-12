400b9fa0b1efe9de4e17e19eb8672108f9cc73a1
Wes <wes_dillingham@harvard.edu>
2017-05-12 08:24:06 -0400

    6

--- final/s3_polly_glacier.py
+++ final/s3_polly_glacier.py
@@ -8,7 +8,7 @@ s3_client = boto3.client('s3')
 cf_client = boto3.client('cloudformation')
 glacier_client = boto3.client('glacier')
 cf_bucket = 'cloudformation-input-bucket'
-target_bucket = 'neighborhood-development-translation-bucket5'
+target_bucket = 'neighborhood-development-translation-bucket6'
 
 s3_resource.create_bucket(Bucket=cf_bucket) #this is the bucket where cloudformation JSON objects live
 data = open('s3_cf.json', 'rb')
@@ -17,7 +17,7 @@ s3_resource.Bucket(cf_bucket).put_object(Key='s3_cf.json', Body=data)
 
 # create the stack (which in turn creates an s3 bucket for future polly files)
 cf_response = cf_client.create_stack(
-    StackName='neighborhood-development-stack5',
+    StackName='neighborhood-development-stack6',
     TemplateURL='https://s3.amazonaws.com/cloudformation-input-bucket/s3_cf.json'
     )
 time.sleep(60)
